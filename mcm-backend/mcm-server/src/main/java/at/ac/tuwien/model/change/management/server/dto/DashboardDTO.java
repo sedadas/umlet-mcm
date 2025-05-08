5121fee2bd018fbb250c1296cf4a36f05729a209
Ivaylo Bonev <e12025894@student.tuwien.ac.at>
2025-05-08 15:07:15 +0300

    fix: #31, #32, #65: Fixed DashboardDTO annotation.

--- mcm-backend/mcm-server/src/main/java/at/ac/tuwien/model/change/management/server/dto/DashboardDTO.java
+++ mcm-backend/mcm-server/src/main/java/at/ac/tuwien/model/change/management/server/dto/DashboardDTO.java
@@ -1,12 +1,12 @@
 package at.ac.tuwien.model.change.management.server.dto;
 
 import jakarta.annotation.Nullable;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.util.List;
 
 public record DashboardDTO(
         @Nullable String id,
-        @NotNull List<UserRoleDTO> allowedRoles
+        @NotEmpty List<UserRoleDTO> allowedRoles
 ) {
 }
